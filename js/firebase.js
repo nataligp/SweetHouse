17b5ffcc2b0ec7041e3eaf80b9e536944a0511d0
nataligp <nattygpsz@gmail.com>
2023-04-18 21:00:12 -0300

    GESTAO DE ACESSO

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMb0DPpdt9o0ifqQU9cCkwFuXECbrF8Vk",
+  authDomain: "sweethouse-9ad78.firebaseapp.com",
+  projectId: "sweethouse-9ad78",
+  storageBucket: "sweethouse-9ad78.appspot.com",
+  messagingSenderId: "102544274088",
+  appId: "1:102544274088:web:a82d50d33c30138bea089d"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ js/mascara.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#CEP').mask('99999-999')
+    $('#telefone').mask('(99) 99999-9999')
+    $('#telefonefixo').mask('(99) 9999-9999')
+})
